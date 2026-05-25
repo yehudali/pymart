f6474be6a6759bb8638b9e2eac1fb6d1a761ba64
yehuda linker <yehuda.linker@gmail.com>
2026-05-25 13:12:26 +0300

    add delete_product_from_cart1 func

--- services/cart_servise/app/service/service.py
+++ services/cart_servise/app/service/service.py
@@ -1,5 +1,5 @@
 from app.schemas.product import CreateItemDTO
-from app.repositories.redis_crud import  product_exists, save_product_in_cart, update_product_quantity
+from app.repositories.redis_crud import  product_exists, save_product_in_cart, update_product_quantity, delete_product_from_cart, delete_cart
 import redis.asyncio
 
 async def add_or_update_product_to_user_cart(user_id:str, product_id:str, data:CreateItemDTO, redis_client:redis.asyncio.Redis):
@@ -14,4 +14,22 @@ async def add_or_update_product_to_user_cart(user_id:str, product_id:str, data:C
         return await update_product_quantity(user_id=user_id, product_id=product_id, quantity=data.quantity, redis_client=redis_client)
     
 
+async def delete_product_from_cart1(user_id:str, product_id:str, redis_client:redis.asyncio.Redis):
+    """הפעלת הפונקציה שעובדת מול רדיס, אם החזירה 1 נחיז תרוו אחרת פאלס"""
+    res = await delete_product_from_cart(user_id=user_id, product_id=product_id, redis_client=redis_client)
+    if res:
+        return True
+    else:
+        return False
     
+
+async def delete_cart1(user_id:str, redis_client:redis.asyncio.Redis):
+    res = await delete_cart(user_id=user_id,redis_client=redis_client)
+    if res:
+        return True
+    else:
+        return False
+    
+
+async def update_quantity1(user_id:str, product_id:str, quantity:int, redis_client:redis.asyncio.Redis):
+    return await update_product_quantity(user_id=user_id, product_id=product_id, quantity=quantity, redis_client=redis_client)
