3bff9a46e54c311af471ec87590f62078f09a74e
yehuda linker <yehuda.linker@gmail.com>
2026-05-27 16:59:22 +0300

    שיפור נראות של הסוואגר, ע"י תגים לפי נושאים

--- services/cart_servise/app/routes/cart_router.py
+++ services/cart_servise/app/routes/cart_router.py
@@ -12,12 +12,13 @@ from app.schemas.cart import CartInfoResponse
 
 router = APIRouter()
 
-@router.get("/healthcheck")
+@router.get("/healthcheck", tags=["healthcheck"])
 async def healthcheck_test(redis_client:redis.Redis = Depends(get_redis_client)):
+    """Checking if there is a connection to the REDIS server, returning a boolean response"""
     return await redis_client.ping() # type: ignore
 
 
-@router.post("/cart/product")
+@router.post("/cart/product", tags=["cart"])
 async def add_product_to_cart(product:AddProductToCart, elastic_client:AsyncElasticsearch = Depends(get_elastic_client),user_id=Depends(checking_basic_user_permissions), redis_client:redis.Redis = Depends(get_redis_client)):
 
     """ בדיקה וחיבור לאלסטיק, בכדי לוודאות לפני ההוספה שהמוצר קיים בקטלוג    """
@@ -37,7 +38,7 @@ async def add_product_to_cart(product:AddProductToCart, elastic_client:AsyncElas
          print(f"error: {err}")
          raise HTTPException(status_code=500, detail="something went wrong show logs")
     
-@router.delete("/cart/product")
+@router.delete("/cart/product", tags=["cart"])
 async def delete_product_from_cart(product_id:DeleteProduct, user_id=Depends(checking_basic_user_permissions), redis_client:redis.Redis = Depends(get_redis_client)):
         try:
             return await delete_product_from_cart1(user_id=user_id, product_id=product_id.id, redis_client=redis_client)
@@ -45,7 +46,7 @@ async def delete_product_from_cart(product_id:DeleteProduct, user_id=Depends(che
          print(f"error: {err}")
          raise HTTPException(status_code=500, detail="something went wrong show logs")
         
-@router.delete("/cart")
+@router.delete("/cart", tags=["cart"])
 async def delete_cart(user_id=Depends(checking_basic_user_permissions), redis_client:redis.Redis = Depends(get_redis_client)):
     try:
         return await delete_cart1(user_id=user_id, redis_client=redis_client)
@@ -53,7 +54,7 @@ async def delete_cart(user_id=Depends(checking_basic_user_permissions), redis_cl
          print(f"error: {err}")
          raise HTTPException(status_code=500, detail="something went wrong show logs")
          
-@router.put("/cart/product/quantity")
+@router.put("/cart/product/quantity", tags=["cart"])
 async def update_quantity(update:UpdateQuantityProduct ,user_id=Depends(checking_basic_user_permissions), redis_client:redis.Redis = Depends(get_redis_client)):
     try:
         return await update_quantity1(user_id=user_id, product_id=update.product_id, quantity=update.quantity,redis_client=redis_client)
@@ -61,7 +62,7 @@ async def update_quantity(update:UpdateQuantityProduct ,user_id=Depends(checking
          print(f"error: {err}")
          raise HTTPException(status_code=500, detail="something went wrong show logs")
 
-@router.get("/cart", response_model=CartInfoResponse)
+@router.get("/cart", response_model=CartInfoResponse, tags=["cart"])
 async def get_cart_and_information(user_id = Depends(checking_basic_user_permissions), redis_client:redis.Redis = Depends(get_redis_client)):
     try:
         return await get_cart_product_and_information(user_id=user_id, redis_client=redis_client)
--- services/cart_servise/app/routes/order_router.py
+++ services/cart_servise/app/routes/order_router.py
@@ -7,7 +7,7 @@ from app.service.creating_orders import creating_orders
 
 router = APIRouter()
 
-@router.post("/create_order")
+@router.post("/create_order", tags=["orders"])
 async def create_order(user_id: str = Depends(checking_basic_user_permissions), redis_client = Depends(get_redis_client)):
     """
     ראוט ליצירת הזמנה חדשה, הראוט יקבל את מזהה המשתמש שמנסה ליצור את ההזמנה, ויעביר אותו לפונקציה של יצירת ההזמנה, שתבדוק את העגלה של המשתמש ב-redis, תשלח את המוצרים והכמויות לסרוויס של הקטלוג לבדיקה האם יש מלאי זמין, במידה ויש מלאי זמין, נוכל להמשיך בתהליך יצירת ההזמנה, במידה ואין מלאי זמין, נחזיר הודעה מתאימה ללקוח שהמוצר לא זמין כרגע במלאי ולא ניתן ליצור הזמנה עם מוצר זה.
