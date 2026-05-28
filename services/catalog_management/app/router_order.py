acad1bc0f9e9c06d5487611ab10621d65c83d711
yehuda linker <yehuda.linker@gmail.com>
2026-05-28 16:13:43 +0300

    empty

--- services/catalog_management/app/router_order.py
+++ services/catalog_management/app/router_order.py
@@ -7,7 +7,7 @@ from elasticsearch_file import get_product_by_id, update_product
 router = APIRouter()
 
 
-@router.post("/create_order", tags=['orders'])
+@router.post("/create_order", tags=['order_process'])
 async def inventory_checking_and_updating_process_for_the_purpose_of_placing_an_order(
     order_items: List[OrderItemIncoming], 
     token = Depends(checking_basic_user_permissions)
