46ebcedd33824b70cd18e50fa82708a2576e8b0b
yehuda linker <yehuda.linker@gmail.com>
2026-05-28 12:49:21 +0300

    השלמה של האנדפוינט של יצירת הזמנה, נדרש לכתוב את הפעולות מול רביט אלסטיק ועוד לאחר שההזמנה קרתה

--- services/order_management/app/routes/order_process.py
+++ services/order_management/app/routes/order_process.py
@@ -8,9 +8,18 @@ router = APIRouter()
 @router.post("/orders/", tags=["orders"])
 async def create_new_order(user_id: str = Depends(checking_basic_user_permissions)):
     try:
-        await send_request_to_cart_management_service(user_id=user_id)
-        return {"success": True, "message": "order processing started successfully"}
+        if await send_request_to_cart_management_service(user_id=user_id):
+            try:
+                # TODO הוספת לוגיקה של יצירת הזמנה ב-ES, ופרסום RABBITMQ
+                pass
+
+                return {"message": "order created successfully"}
+            except Exception as err:
+                print(f"order creation failed for user {user_id}.. error: {err}")
+                raise HTTPException(status_code=400, detail="unable to create order at this time")
+        
         
     except Exception as err:
-        print(f"Order creation failed for user {user_id}.. error: {err}")
-        raise HTTPException(status_code=400, detail="unable to process order at this time")
+        print(f"order creation failed for user {user_id}.. error: {err}")
+        raise HTTPException(status_code=400, detail="unable to process order")
+    
