c46c68adc746af0b5bda5a13cdb3ec47507c04ef
yehuda linker <yehuda.linker@gmail.com>
2026-06-02 16:05:37 +0300

    מחיקת העגלה בסרוויס CART לאחר ההזמנה

--- services/cart_servise/app/routes/order_router.py
+++ services/cart_servise/app/routes/order_router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from app.core.redis_client import get_redis_client
 from app.service.creating_orders import create_order_service
-
+from app.service.service import delete_cart1
 
 router = APIRouter()
 
@@ -10,6 +10,7 @@ router = APIRouter()
 async def create_order(user_id: str, redis_client=Depends(get_redis_client)):
     try:
         cart = await create_order_service(user_id=user_id, redis_client=redis_client)
+        await delete_cart1(user_id=user_id, redis_client=redis_client)
         return cart
 
     except ValueError as err:
