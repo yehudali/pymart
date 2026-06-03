0e1d159b0c338e238d20c4b1601375745c43787e
yehuda linker <yehuda.linker@gmail.com>
2026-06-03 13:24:45 +0300

    טיפול בהמרת התאריך לג'ייסון בצורה תקינה.... כרגע ההזמנה נוצרת נשמרת ומתפרסמת בהצלחה

--- services/order_management/app/routes/order_process.py
+++ services/order_management/app/routes/order_process.py
@@ -13,24 +13,27 @@ from app.repositories.rabbitmq_publish import publish_new_order_to_manage_queue
 
 router = APIRouter()
 
+
 @router.post("/create_order", tags=["orders"])
-async def create_new_order(user_id=Depends(checking_basic_user_permissions), elastic_search_client=Depends(get_elastic_search_client), rabbitmq_channel: AbstractChannel = Depends(get_rabbitmq_channel)):
+async def create_new_order(
+    user_id=Depends(checking_basic_user_permissions),
+    elastic_search_client=Depends(get_elastic_search_client),
+    rabbitmq_channel: AbstractChannel = Depends(get_rabbitmq_channel),
+):
     try:
         cart = await send_request_to_cart_management_service(user_id=user_id)
-        new_order = Order(
-        user_id=user_id,
-        cart=cart.cart
-    )   
-        await save_order(elastic_client=elastic_search_client, order=new_order.model_copy().model_dump())
-        await publish_new_order_to_manage_queue(order=new_order.model_copy().model_dump(), rabbitmq_channel=rabbitmq_channel)
-        
-        # TODO הוספת לוגיקה של יצירת הזמנה ב-ES, ופרסום RABBITMQ
+        new_order = Order(user_id=user_id, cart=cart.cart)
 
+        order_dict = new_order.model_dump(mode="json")
+
+        await save_order(elastic_client=elastic_search_client, order=order_dict)
+        await publish_new_order_to_manage_queue(
+            order=order_dict, rabbitmq_channel=rabbitmq_channel
+        )
 
     except Exception as err:
         print(f"order creation failed for user {user_id}.. error: {err}")
         raise HTTPException(status_code=400, detail="unable to process order")
-
 
 
 @router.get("/orders", tags=["orders"])
@@ -42,11 +45,17 @@ async def get_orders(user_id: str = Depends(checking_basic_user_permissions)):
         return {"message": "orders retrieved successfully"}
     except Exception as err:
         print(f"failed to retrieve orders for user {user_id}.. error: {err}")
-        raise HTTPException(status_code=400, detail="unable to retrieve orders at this time")
+        raise HTTPException(
+            status_code=400, detail="unable to retrieve orders at this time"
+        )
 
 
 @router.put("/orders/{order_id}/status", tags=["orders"])
-async def update_order_status(order_id: str, new_status: str, user_id: str = Depends(checking_basic_user_permissions)):
+async def update_order_status(
+    order_id: str,
+    new_status: str,
+    user_id: str = Depends(checking_basic_user_permissions),
+):
     try:
         # TODO הוספת לוגיקה של עדכון סטטוס הזמנה ב-ES, ופרסום RABBITMQ
         pass
@@ -54,5 +63,6 @@ async def update_order_status(order_id: str, new_status: str, user_id: str = Dep
         return {"message": "order status updated successfully"}
     except Exception as err:
         print(f"order status update failed for order {order_id}.. error: {err}")
-        raise HTTPException(status_code=400, detail="unable to update order status at this time")
-    
+        raise HTTPException(
+            status_code=400, detail="unable to update order status at this time"
+        )
