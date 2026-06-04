5899e3da5aec661dbfb4408978f1389d537337df
yehuda linker <yehuda.linker@gmail.com>
2026-06-04 16:46:49 +0300

    השלמת השם יוזר מנג'ר במקום יוזר סרוויס

--- services/order_management/app/core/config.py
+++ services/order_management/app/core/config.py
@@ -13,7 +13,7 @@ class Settings(BaseSettings):
     RABBITMQ_PORT: int = 5672
 
     ELASTICSEARCH_URL: str = "http://elasticsearch:9200"
-    USER_MANAGEMENT_URL: str = "http://user-service:8001"
+    USER_MANAGEMENT_URL: str = "http://user-management:8001"
     CART_SERVICE_URL:str = "http://cart-service:8003"
     
 
